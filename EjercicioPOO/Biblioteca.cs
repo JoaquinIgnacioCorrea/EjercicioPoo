df91ff479a42f807420bf870a56fee8902151be6
Joaquin Ignacio Correa <joaquincorrea006@gmail.com>
2023-12-03 18:28:01 -0300

    Se realizan las actividades del repositorio.

---
+++ EjercicioPOO/Biblioteca.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPoo
+{
+    class Biblioteca
+    {
+        public List<Libro> ListaLibros { get; set; }
+
+        public Biblioteca()
+        {
+            ListaLibros = new List<Libro>();
+        }
+
+        public void PrestarLibro(string libro, string estudiante)
+        {
+            Libro LibroEncontrado = ListaLibros.Find(LibroBuscado => LibroBuscado.Titulo == libro);
+
+            if(LibroEncontrado.Prestado == false) 
+            {
+                Console.WriteLine($"Libro {LibroEncontrado.Titulo} prestado a {estudiante}.");
+                LibroEncontrado.Prestado = true; 
+            }
+            else Console.WriteLine("El libro seleccionado no esta disponible.");
+        }
+
+        public void DevolverLibro(string libro, string estudiante)
+        {
+            Libro LibroEncontrado = ListaLibros.Find(LibroBuscado => LibroBuscado.Titulo == libro);
+
+            if (LibroEncontrado.Prestado == true)
+            {
+                Console.WriteLine($"Libro {LibroEncontrado.Titulo} devueldo del estudiante {estudiante}.");
+                LibroEncontrado.Prestado = false;
+            }
+            else Console.WriteLine("El libro esta disponible, no puede devolverse.");
+        }
+
+        public void ListarLibros()
+        {
+            Console.WriteLine("Libros Disponibles:\n");
+            foreach (var libro in ListaLibros)
+            {
+                if(libro.Prestado == false) Console.WriteLine($"Libro: {libro.Titulo}\nAutor: {libro.Autor}\nDescripcion: {libro.Descripcion}\n\n");
+            }
+            Console.WriteLine("Libros Prestados:\n");
+            foreach (var libro in ListaLibros)
+            {
+                if (libro.Prestado == true) Console.WriteLine($"Libro: {libro.Titulo}\nAutor: {libro.Autor}\nDescripcion: {libro.Descripcion}\n\n");
+            }
+        }
+
+        public void ListarLibrosPrestadosAlumno(string alumno)
+        {
+            StringBuilder ListadoLibrosPrestados = new StringBuilder();
+            var cant = ListaLibros.Count(libro => libro.Prestado == true);
+            foreach(var libros in ListaLibros)
+            {
+                if(libros.Prestado == true)
+                {
+                    ListadoLibrosPrestados.Append($"Titulo: {libros.Titulo}\nAutor: {libros.Autor}\nDescripcion: {libros.Descripcion}\n\n");
+                }
+            }
+            Console.WriteLine($"El estudiante {alumno} tiene {cant} libros prestados:\n{ListadoLibrosPrestados}");
+        }
+
+        public void AgregarLibro(Libro libroAdd)
+        {
+            ListaLibros.Add(libroAdd);
+        }
+    }
+}
--- EjercicioPOO/Program.cs
+++ EjercicioPOO/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace EjercicioPoo
@@ -8,20 +11,38 @@ namespace EjercicioPoo
         static void Main()
         {
             #region Carga de datos
-            string data = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"data.json")   );
-                  
+            string data = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"data.json"));
+
             #endregion
-            
+            #region Normalizacion de Variables
+            var normalizacionVariables = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            #endregion
+
             //Gestionar los prestamos de libros de una biblioteca
             // se debe poder prestar libros a estudiantes: 
             //  *  solo hay una copia por libro y hay que verificar a la hora de prestarlo si lo tenemos disponible o no
             // Se debe poder devolver libros 
-            // Se debe poder listar todos los libros  y los que se prestaron
+            // Se debe poder listar todos los libros y los que se prestaron
             // Se debe poder consultar los libros que tiene prestado un estudiante en particular
 
+            List<Libro> ColeccionLibros = JsonSerializer.Deserialize<List<Libro>>(data,normalizacionVariables);
+            Biblioteca BibliotecaEj = new Biblioteca();
+            foreach (var Libros in ColeccionLibros) BibliotecaEj.AgregarLibro(Libros);
 
+            BibliotecaEj.PrestarLibro("Cien años de soledad", "Joaquin");
+            BibliotecaEj.PrestarLibro("1984", "Joaquin");
+            BibliotecaEj.DevolverLibro("Harry Potter y la piedra filosofal", "Joaquin");
+            BibliotecaEj.DevolverLibro("Crónica de una muerte anunciada", "Joaquin");
+            BibliotecaEj.ListarLibros();
+            BibliotecaEj.ListarLibrosPrestadosAlumno("Joaquin");
+
+
+
+        Console.ReadKey();
         }
-
 
     }
 }
